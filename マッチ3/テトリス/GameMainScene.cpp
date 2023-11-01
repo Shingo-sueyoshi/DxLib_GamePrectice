2550feca398647531c7fda09268f28f2f50188d2
Shingo-sueyoshi <138412610+Shingo-sueyoshi@users.noreply.github.com>
2023-11-01 10:12:27 +0900

    テトリス

---
+++ マッチ3/テトリス/GameMainScene.cpp
@@ -0,0 +1,19 @@
+#include "GameMainScene.h"
+#include "DxLib.h"
+#include "Block.h"
+#include "SceneManager.h"
+
+/*
+マクロ定義
+*/
+
+
+/*
+型定義
+*/
+
+
+/*
+グローバル変数宣言
+*/
+int BackGround_image;      //背景画像イメージ
